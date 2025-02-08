5972c7723164fedfc8bcd1860713cd1a7a588f40
Justin <justinguerrerowork@gmail.com>
2025-02-08 18:47:59 -0500

    move shadow with bouncing sphere

--- shadows/src/script.js
+++ shadows/src/script.js
@@ -7,6 +7,9 @@ import GUI from 'lil-gui'
 const textureLoader = new THREE.TextureLoader()
 const bakedShadow = textureLoader.load('/textures/bakedShadow.jpg')
 bakedShadow.colorSpace = THREE.SRGBColorSpace
+
+const simpleShadow = textureLoader.load('/textures/simpleShadow.jpg')
+simpleShadow.colorSpace = THREE.SRGBColorSpace
 /**
  * Base
  */
@@ -102,7 +105,7 @@ sphere.castShadow = true
 
 const plane = new THREE.Mesh(
     new THREE.PlaneGeometry(5, 5),
-    new THREE.MeshBasicMaterial({map:bakedShadow})
+    material
 )
 plane.receiveShadow = true
 
@@ -110,6 +113,18 @@ plane.rotation.x = - Math.PI * 0.5
 plane.position.y = - 0.5
 
 scene.add(sphere, plane)
+
+const sphereShadow = new THREE.Mesh(
+    new THREE.PlaneGeometry(1.5,1.5),
+    new THREE.MeshBasicMaterial({
+        color:0x000000,
+        transparent: true,
+        alphaMap: simpleShadow
+    })
+)
+sphereShadow.rotation.x = -Math.PI * 0.5
+sphereShadow.position.y = plane.position.y + 0.01
+scene.add(sphereShadow)
 
 /**
  * Sizes
@@ -168,6 +183,16 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    //Update the Sphere
+    sphere.position.x = Math.cos(elapsedTime) * 1.5
+    sphere.position.z = Math.sin(elapsedTime) * 1.5
+    sphere.position.y = Math.abs(Math.sin(elapsedTime * 3))
+
+    //Update sphereShadow
+    sphereShadow.position.x = sphere.position.x
+    sphereShadow.position.z = sphere.position.z
+    sphereShadow.material.opacity = (1 - sphere.position.y) * 1.2
 
     // Update controls
     controls.update()
