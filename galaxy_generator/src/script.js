0d377de0d32403f09390f8cac9fcec0728656a6b
Justin <justinguerrerowork@gmail.com>
2025-02-11 10:53:22 -0500

    geometry and branches

--- galaxy_generator/src/script.js
+++ galaxy_generator/src/script.js
@@ -14,14 +14,73 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-/**
- * Test cube
- */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
-)
-scene.add(cube)
+//Galaxy
+const parameters = {}
+parameters.count = 100000
+parameters.size = 0.01
+parameters.radius = 5
+parameters.branches = 3
+
+let geometry = null
+let material = null
+let points = null
+
+
+const generateGalaxy = () =>
+{
+    //Destroy old galaxy
+    if (points !== null){
+        geometry.dispose()
+        material.dispose()
+        scene.remove(points)
+    }
+    /**
+     * Geometry
+     */
+   geometry = new THREE.BufferGeometry()
+
+    const positions = new Float32Array(parameters.count * 3)
+
+    for(let i = 0; i < parameters.count; i++)
+    {
+        const i3 = i * 3
+
+        const radius = Math.random() * parameters.radius
+        const branchAngle = (i % parameters.branches)/ parameters.branches * Math.PI * 2
+
+        positions[i3 + 0] = Math.cos(branchAngle) * radius
+        positions[i3 + 1] = 0
+        positions[i3 + 2] = Math.sin(branchAngle) * radius
+    }
+
+    geometry.setAttribute(
+        'position', 
+        new THREE.BufferAttribute(positions, 3)
+    )
+
+    /**
+     * Material
+     */
+    material = new THREE.PointsMaterial({
+        size: parameters.size,
+        sizeAttenuation: true,
+        depthWrite: false,
+        blending: THREE.AdditiveBlending
+    })
+
+    /**
+     * Points
+     */
+    points = new THREE.Points(geometry, material)
+    scene.add(points)
+}
+generateGalaxy()
+
+gui.add(parameters, 'count').min(100).max(1000000).step(100).onFinishChange(generateGalaxy)
+gui.add(parameters, 'size').min(0.001).max(0.1).step(0.001).onFinishChange(generateGalaxy)
+gui.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy)
+gui.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange(generateGalaxy)
+
 
 /**
  * Sizes
