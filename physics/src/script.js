c56d7e24b4fcc102d3a1ed9f065dade42a622e90
Justin <justinguerrerowork@gmail.com>
2025-02-18 11:32:15 -0500

    createBox

--- physics/src/script.js
+++ physics/src/script.js
@@ -9,6 +9,38 @@ import CANNON from 'cannon'
 const gui = new GUI()
 gui.close()
 
+const debugObject = {
+
+}
+
+debugObject.createSphere =()=>{
+    createSphere(
+        Math.random() * 0.5,
+        {
+            x:(Math.random() - 0.5) * 3,
+            y:3,
+            z:(Math.random() - 0.5) * 3,
+        }
+    )
+}
+gui.add(debugObject,'createSphere')
+
+debugObject.createBox = () =>
+{
+    createBox(
+        Math.random(),
+        Math.random(),
+        Math.random(),
+        {
+            x: (Math.random() - 0.5) * 3,
+            y: 3,
+            z: (Math.random() - 0.5) * 3
+        }
+    )
+}
+gui.add(debugObject, 'createBox')
+
+
 /**
  * Base
  */
@@ -177,21 +209,25 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 
 const objectsToUpdate = [
-
-
+    
 ]
+
+/**Sphere
+ * 
+ */
+
+const sphereGeometry = new THREE.SphereGeometry(1, 20,20)
+const sphereMaterial = new THREE.MeshStandardMaterial({
+    metalness: 0.3,
+    roughness: 0.4,
+    envMap: environmentMapTexture,
+})
 
 const createSphere = (radius, position) => 
 {
     //Three.js Mesh
-    const mesh = new THREE.Mesh(
-        new THREE.SphereGeometry(radius, 20,20),
-        new THREE.MeshStandardMaterial({
-            metalness: 0.3,
-            roughness: 0.4,
-            envMap: environmentMapTexture,
-        })
-    )
+    const mesh = new THREE.Mesh(sphereGeometry,sphereMaterial)
+    mesh.scale.set(radius, radius,radius)
     mesh.castShadow = true
     mesh.position.copy(position)
     scene.add(mesh)
@@ -217,8 +253,45 @@ const createSphere = (radius, position) =>
 
 
 
-createSphere(0.5,{x:0,y:3,z:0})
+// createSphere(0.5,{x:0,y:3,z:0})
 
+/**
+ * Boxes
+ */
+
+const boxGeometry = new THREE.BoxGeometry(1,1,1)
+const boxMaterial = new THREE.MeshStandardMaterial({
+    metalness:0.3,
+    roughness:0.4,
+    envMap: environmentMapTexture,
+    envMapIntensity: 0.5
+})
+
+const createBox = (width,height,depth,position)=>{
+
+    //Three.js mesh
+    const mesh = new THREE.Mesh(boxGeometry,boxMaterial)
+    mesh.scale.set(width,height,depth)
+    mesh.castShadow = true
+    mesh.position.copy(position)
+    scene.add(mesh)
+
+    //Cannon body
+    const shape = new CANNON.Box(new CANNON.Vec3(width * 0.5, height * 0.5, depth * 0.5))
+    const body = new CANNON.Body({
+        mass: 1,
+        position: new CANNON.Vec3(0,3,0),
+        shape: shape,
+        material: defaultMaterial
+    })
+    
+    body.position.copy(position)
+    world.addBody(body)
+
+    objectsToUpdate.push({mesh,body})
+}
+
+createBox(1,1.5,2,{ x: 0, y: 3, z: 0 })
 /**
  * Animate
  */
@@ -237,8 +310,10 @@ const tick = () =>
 
     world.step(1/60,deltaTime,3)
 
-    for(const object of objectsToUpdate){
+    for(const object of objectsToUpdate)
+    {
         object.mesh.position.copy(object.body.position)
+        object.mesh.quaternion.copy(object.body.quaternion)
     }
 
     // sphere.position.copy(sphereBody.position)
