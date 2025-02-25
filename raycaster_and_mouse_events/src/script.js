ca9b3bc70b51a753ac77ec4c36848686ec9f7816
Justin <justinguerrerowork@gmail.com>
2025-02-25 11:18:53 -0500

    raycaster intersect object and objects

--- raycaster_and_mouse_events/src/script.js
+++ raycaster_and_mouse_events/src/script.js
@@ -34,7 +34,33 @@ const object3 = new THREE.Mesh(
 )
 object3.position.x = 2
 
+// Three.js updates the objects’ coordinates (called matrices) right before rendering them. SincYou can fix that by updating the matrices manually before ray casting:
+
+
+object1.updateMatrixWorld()
+object2.updateMatrixWorld()
+object3.updateMatrixWorld()
+
 scene.add(object1, object2, object3)
+
+/**
+ * RayCaster
+ */
+
+//need origin and direction of ray
+const raycaster = new THREE.Raycaster();
+
+const rayOrigin = new THREE.Vector3(-3,0,0)
+const rayDirection = new THREE.Vector3(10,0,0)
+rayDirection.normalize() //normalizes length of rayDirection to 1 while maintaining direction
+
+raycaster.set(rayOrigin,rayDirection)
+
+const intersect = raycaster.intersectObject(object2)
+console.log(intersect)
+
+const intersects = raycaster.intersectObjects([object1, object2, object3])
+console.log(intersects)
 
 /**
  * Sizes
