30bb92f056d20aee9eaeb7ddd872d6856ec2fb3f
Justin <justinguerrerowork@gmail.com>
2025-02-25 11:28:10 -0500

    change material on raycaster intersect

--- raycaster_and_mouse_events/src/script.js
+++ raycaster_and_mouse_events/src/script.js
@@ -50,17 +50,7 @@ scene.add(object1, object2, object3)
 //need origin and direction of ray
 const raycaster = new THREE.Raycaster();
 
-const rayOrigin = new THREE.Vector3(-3,0,0)
-const rayDirection = new THREE.Vector3(10,0,0)
-rayDirection.normalize() //normalizes length of rayDirection to 1 while maintaining direction
 
-raycaster.set(rayOrigin,rayDirection)
-
-const intersect = raycaster.intersectObject(object2)
-console.log(intersect)
-
-const intersects = raycaster.intersectObjects([object1, object2, object3])
-console.log(intersects)
 
 /**
  * Sizes
@@ -114,6 +104,32 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    //Animate objects
+    object1.position.y = Math.sin(elapsedTime * 0.3) *1.5
+    object2.position.y = Math.sin(elapsedTime * 0.8) *1.5 
+    object3.position.y = Math.sin(elapsedTime * 1.4) *1.5
+
+    //Cast a ray 
+    const rayOrigin = new THREE.Vector3(-3,0,0)
+    const rayDirection = new THREE.Vector3(1,0,0)
+    rayDirection.normalize()
+
+    raycaster.set(rayOrigin,rayDirection)
+
+    const objectsToTest = [object1,object2,object3]
+    const intersects  = raycaster.intersectObjects(objectsToTest)
+    console.log(intersects.length)
+
+    for(const object of objectsToTest)
+    {
+        object.material.color.set('#ff0000')
+    }
+
+    for(const intersect of intersects)
+    {
+        intersect.object.material.color.set('#0000ff')
+    }
 
     // Update controls
     controls.update()
