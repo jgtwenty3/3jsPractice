120ec564acd7f1ddc781251de64560c34a89525c
Justin <justinguerrerowork@gmail.com>
2025-03-01 17:51:16 -0500

    renderer set up

--- code_structuring/src/Experience/Experience.js
+++ code_structuring/src/Experience/Experience.js
@@ -2,12 +2,22 @@ import * as THREE from 'three'
 
 import Sizes from "./Utils/Sizes"
 import Time from "./Utils/Time"
+import Camera from './Camera'
+import Renderer from './Renderer'
+
+let instance = null
 
 
 
 export default class Experience{
 	constructor(canvas){
 		
+		if(instance){
+			return(instance)
+		}
+
+		instance = this
+
 		window.experience = this
 
         // Options
@@ -18,6 +28,8 @@ export default class Experience{
         this.sizes = new Sizes()
         this.time = new Time()
         this.scene = new THREE.Scene()
+		this.camera = new Camera()
+		this.renderer = new Renderer()
        
         // Resize event
         this.sizes.on('resize', () =>
@@ -32,10 +44,14 @@ export default class Experience{
         })
 	}
 	resize(){
+		this.camera.resize()
+		this.renderer.resize()
 
 	}
 	update(){
-		
+		this.camera.update()
+		this.renderer.update()
+
 	}
 	
 }
