5eba55470030249004034880adf270d872ded63a
Justin <justinguerrerowork@gmail.com>
2025-03-02 13:07:36 -0500

    finished code structuring

--- code_structuring/src/Experience/World/Environment.js
+++ code_structuring/src/Experience/World/Environment.js
@@ -7,6 +7,13 @@ export default class Environment{
 		this.experience = new Experience()
 		this.scene = this.experience.scene
 		this.resources = this.experience.resources
+		this.debug = this.experience.debug
+
+		//debug
+		if(this.debug.active)
+        {
+            this.debugFolder = this.debug.ui.addFolder('environment')
+        }
 
 		this.setSunLight()
 		this.setEnivronmentMap()
@@ -20,6 +27,39 @@ export default class Environment{
 		this.sunLight.shadow.normalBias = 0.05
 		this.sunLight.position.set(3,3,-2.25)
 		this.scene.add(this.sunLight)
+
+		if(this.debug.active)
+        {
+            this.debugFolder
+                .add(this.sunLight, 'intensity')
+                .name('sunLightIntensity')
+                .min(0)
+                .max(10)
+                .step(0.001)
+            
+            this.debugFolder
+                .add(this.sunLight.position, 'x')
+                .name('sunLightX')
+                .min(- 5)
+                .max(5)
+                .step(0.001)
+            
+            this.debugFolder
+                .add(this.sunLight.position, 'y')
+                .name('sunLightY')
+                .min(- 5)
+                .max(5)
+                .step(0.001)
+            
+            this.debugFolder
+                .add(this.sunLight.position, 'z')
+                .name('sunLightZ')
+                .min(- 5)
+                .max(5)
+                .step(0.001)
+        }
+
+		
 	}
 
 	setEnivronmentMap(){
@@ -27,6 +67,17 @@ export default class Environment{
 		this.environmentMap.intensity = 0.4
 		this.environmentMap.texture = this.resources.items.environmentMapTexture
 		this.environmentMap.texture.colorSpace = THREE.SRGBColorSpace	
+
+		if(this.debug.active)
+        {
+            this.debugFolder
+                .add(this.environmentMap, 'intensity')
+                .name('envMapIntensity')
+                .min(0)
+                .max(4)
+                .step(0.001)
+                .onChange(this.environmentMap.updateMaterials)
+        }
 
 		this.scene.environment = this.environmentMap.texture
 
